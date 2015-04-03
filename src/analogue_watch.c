e19a1b654463338898b677783c2dcd5be0258f32
SAkira <s.akira@me.com>
2015-04-03 19:41:20 +0900

    release 1.1

--- src/analogue_watch.c
+++ src/analogue_watch.c
@@ -4,8 +4,9 @@
 #define Key_BackgroundColor 0
 #define Key_Interval 1
 #define Key_HourDigit 2
-static int s_backgroundColor_white = 1, 
-  s_interval_second = 1, s_hourDigit_show = 1;
+static int s_interval_second = 1, s_hourDigit_show = 1;
+static GColor s_background_color = GColorClear;
+static GColor s_foreground_color = GColorClear;
 
 static Window *s_main_window;
 static Layer *s_main_layer;
@@ -18,6 +19,7 @@ typedef struct {
 static WatchTime s_current_time = {
   .hour = -2, .minute = -2, .second = -2
 };
+static bool s_force_update;
 
 static const int DivisionLength = 120;
 #define HandLength 114
@@ -35,20 +37,22 @@ static const GPathInfo SecondPathInfo = {
     {5, 0}, {-5, 0}}
 };
 
-static void prepare_drawing_hand_division(Layer * const layer, GContext * const ctx, 
+static void prepare_drawing_hand_division(Layer * const layer, GContext * const ctx,
   const int hour24, const int minute);
 static void draw_hand_outline(Layer * const layer, GContext * const ctx);
 static void draw_hand_filled(Layer * const layer, GContext * const ctx, const int second);
-static void draw_division(Layer * const layer, GContext * const ctx, 
+static void draw_division(Layer * const layer, GContext * const ctx,
         const int minute, const bool filled);
 static void draw_divisions(Layer * const layer, GContext * const ctx,
 			  const int hour24, const int minute);
+static void draw_hour_digit(Layer * const Layer, GContext * const ctx,
+        const int hour24, const int minute);
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed);
 static void read_persist_values(void);
 
 // drawing
 
-static void prepare_drawing_hand_division(Layer * const layer, GContext * const ctx, 
+static void prepare_drawing_hand_division(Layer * const layer, GContext * const ctx,
   const int hour24, const int minute) {
   const GRect rect = layer_get_bounds(layer);
 
@@ -58,33 +62,32 @@ static void prepare_drawing_hand_division(Layer * const layer, GContext * const 
   const int hour12 = hour24 % 12;
   const int mh2 = minute * 2 + am_flag * hour12;
   const int mh2_angle = mh2 * TRIG_MAX_ANGLE / 2 / 60;
-  int x =  - (int)(DivisionLength * 3 / 4 * sin_lookup(mh2_angle) / TRIG_MAX_RATIO) 
+  int x =  - (int)(DivisionLength * 3 / 4 * sin_lookup(mh2_angle) / TRIG_MAX_RATIO)
       + rect.size.w / 2;
-  int y =    (int)(DivisionLength * 3 / 4 * cos_lookup(mh2_angle) / TRIG_MAX_RATIO) 
+  int y =    (int)(DivisionLength * 3 / 4 * cos_lookup(mh2_angle) / TRIG_MAX_RATIO)
       + rect.size.h / 2;
-//  APP_LOG(APP_LOG_LEVEL_INFO, "minute=%d angle=%d x=%d y=%d", minute, (int)(angle * 100), x, y);
-//  const int margin = (rect.size.w - DivisionLength) / 2;
   const int margin = -2;
   if (x < -margin) x = -margin;
   if (y < -margin) y = -margin;
   if (x >= rect.size.w + margin) x = rect.size.w + margin - 1;
   if (y >= rect.size.h + margin) y = rect.size.h + margin - 1;
   s_hand_shift = GPoint(x, y);
-  
-  graphics_context_set_fill_color(ctx, GColorWhite);
+
+  graphics_context_set_fill_color(ctx, s_background_color);
   graphics_fill_rect(ctx, layer_get_bounds(layer), 0, GCornerNone);
 }
 
 static void draw_hand_outline(Layer * const layer, GContext * const ctx) {
-  graphics_context_set_stroke_color(ctx, GColorBlack);
+  graphics_context_set_stroke_color(ctx, s_foreground_color);
   gpath_rotate_to(s_hand_path_ptr, s_hand_angle);
   gpath_move_to(s_hand_path_ptr, s_hand_shift);
   gpath_draw_outline(ctx, s_hand_path_ptr);
 }
 
-static void draw_division(Layer * const layer, GContext * const ctx, 
+static void draw_division(Layer * const layer, GContext * const ctx,
   const int minute, const bool filled) {
-  graphics_context_set_stroke_color(ctx, GColorBlack);
+  graphics_context_set_stroke_color(ctx, s_foreground_color);
+  graphics_context_set_fill_color(ctx, s_foreground_color);
   const int angle = minute * TRIG_MAX_ANGLE / 60;
   const int x = (int)(  DivisionLength * sin_lookup(angle) / TRIG_MAX_RATIO) + s_hand_shift.x;
   const int y = (int)(- DivisionLength * cos_lookup(angle) / TRIG_MAX_RATIO) + s_hand_shift.y;
@@ -96,13 +99,34 @@ static void draw_division(Layer * const layer, GContext * const ctx,
 }
 
 static void draw_hand_filled(Layer * const layer, GContext * const ctx, const int second) {
-  const int len = second * HandLength / 60;
+  const int len = second * HandLength / (60 - 1);
   s_second_path_ptr->points[0].y = -len;
   s_second_path_ptr->points[1].y = -len;
-  graphics_context_set_fill_color(ctx, GColorBlack);
+  graphics_context_set_fill_color(ctx, s_foreground_color);
   gpath_rotate_to(s_second_path_ptr, s_hand_angle);
   gpath_move_to(s_second_path_ptr, s_hand_shift);
   gpath_draw_filled(ctx, s_second_path_ptr);
+}
+
+static void draw_hour_digit(Layer * const Layer, GContext * const ctx,
+        const int hour24, const int minute) {
+  const int digit_dist = DivisionLength * 3 / 4;
+  const int radius = 10;
+  const int angle = minute * TRIG_MAX_ANGLE / 60;
+  static char digit_str[2];
+  // circle
+  const int cx = (int)(  digit_dist * sin_lookup(angle) / TRIG_MAX_RATIO) + s_hand_shift.x;
+  const int cy = (int)(- digit_dist * cos_lookup(angle) / TRIG_MAX_RATIO) + s_hand_shift.y;
+  graphics_context_set_fill_color(ctx, s_background_color);
+  graphics_fill_circle(ctx, GPoint(cx, cy), radius);
+  graphics_context_set_stroke_color(ctx, s_foreground_color);
+  graphics_draw_circle(ctx, GPoint(cx, cy), radius);
+  // digit
+  snprintf(digit_str, 2, "%d", hour24 % 12);
+  graphics_context_set_text_color(ctx, s_foreground_color);
+  graphics_draw_text(ctx, digit_str, fonts_get_system_font(FONT_KEY_GOTHIC_14),
+    GRect(cx - radius * 2, cy - radius, radius * 4, radius * 2),
+    GTextOverflowModeFill, GTextAlignmentCenter, NULL);
 }
 
 static void draw_divisions(Layer * const layer, GContext * const ctx,
@@ -112,7 +136,7 @@ static void draw_divisions(Layer * const layer, GContext * const ctx,
   if (is_am) {
     for (int i = minute - 12; i < minute - hour12 + 1; ++ i)
       draw_division(layer, ctx, (i + 60) % 60, false);
-    for (int i = minute - hour12 + 1; i < minute; ++ i) 
+    for (int i = minute - hour12 + 1; i < minute; ++ i)
       draw_division(layer, ctx, (i + 60) % 60, true);
     draw_division(layer, ctx, minute, hour12 > 0);
     for (int i = minute + 1; i <= minute + 12; ++ i)
@@ -135,17 +159,25 @@ static void update_main_layer(Layer *layer, GContext *ctx) {
   if (s_current_time.hour < 0) return;
   //APP_LOG(APP_LOG_LEVEL_INFO, "update_main_layer()");
 
-  if ((last_time.minute != s_current_time.minute) ||
+  if (s_force_update ||
+      (last_time.minute != s_current_time.minute) ||
       (last_time.hour != s_current_time.hour)) {
-    prepare_drawing_hand_division(layer, ctx, 
+    prepare_drawing_hand_division(layer, ctx,
       s_current_time.hour, s_current_time.minute);
     draw_hand_outline(layer, ctx);
-    draw_hand_filled(layer, ctx, s_current_time.second);
     draw_divisions(layer, ctx, s_current_time.hour, s_current_time.minute);
+    if (s_interval_second)
+      draw_hand_filled(layer, ctx, s_current_time.second);
+    else
+      draw_hand_filled(layer, ctx, 60 - 1);
   } else if (last_time.second != s_current_time.second) {
     draw_hand_filled(layer, ctx, s_current_time.second);
   }
+  if (s_hourDigit_show)
+    draw_hour_digit(layer, ctx, s_current_time.hour, s_current_time.minute);
+
   last_time = s_current_time;
+  s_force_update = false;
 }
 
 static void create_rect_pathes(void) {
@@ -155,9 +187,9 @@ static void create_rect_pathes(void) {
 
 static void destroy_rect_pathes(void) {
   if (s_hand_path_ptr)
-    gpath_destroy(s_hand_path_ptr);  
+    gpath_destroy(s_hand_path_ptr);
   if (s_second_path_ptr)
-    gpath_destroy(s_second_path_ptr);  
+    gpath_destroy(s_second_path_ptr);
 }
 
 static void main_window_load(Window *window) {
@@ -214,14 +246,28 @@ static void outbox_sent_callback(DictionaryIterator *iterator, void *context) {
 }
 
 static void read_persist_values(void) {
-  s_backgroundColor_white = persist_read_int(Key_BackgroundColor);
+  if (persist_read_int(Key_BackgroundColor)) {
+    // background color is white
+    s_background_color = GColorWhite;
+    s_foreground_color = GColorBlack;
+  } else {
+    // background color is black
+    s_background_color = GColorBlack;
+    s_foreground_color = GColorWhite;
+  }
   s_interval_second = persist_read_int(Key_Interval);
+  tick_timer_service_subscribe(
+    s_interval_second ? SECOND_UNIT : MINUTE_UNIT,
+    tick_handler);
+
   s_hourDigit_show = persist_read_int(Key_HourDigit);
+  s_force_update = true;
 }
 
 // main
 
 static void init(void) {
+  s_force_update = true;
   create_rect_pathes();
   s_main_window = window_create();
   if (!s_main_window) return;
@@ -230,7 +276,6 @@ static void init(void) {
     .unload = main_window_unload,
   });
   window_stack_push(s_main_window, true);
-  tick_timer_service_subscribe(SECOND_UNIT, tick_handler);
 
   read_persist_values();
 
